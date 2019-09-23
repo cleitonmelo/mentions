559a3a2028dfa448cadb15318e4cf97782b84e61
Cleiton Melo da Silva <cleitonsilva@ext.cvccorp.com.br>
2019-09-23 16:33:59 -0300

    Commit inicial

---
+++ src/controllers/mentions-controller.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Mentions = mongoose.model('Mentions');
+
+// Lista total
+exports.listMentions = async (req, res) => {
+    try {
+      const data = await Mentions.find({});
+      res.status(200).send(data);
+    } catch (e) {
+      res.status(500).send({message: 'Falha ao carregar as menções.'});
+    };
+};
+
+// Criação
+exports.createMention = async (req, res) => {
+    try {
+        const mention = new Mentions({
+            friend: req.body.friend,
+            mention: req.body.mention
+        });
+        await mention.save();
+        res.status(201).send({message: 'Menção cadastrada com sucesso!'});
+    } catch (e) {
+        res.status(500).send({message: 'Falha ao cadastrar a menção.'});  
+  };
+};
