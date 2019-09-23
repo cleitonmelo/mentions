9ca96d7a13809267fa9581c6b722e9707c7b29e6
Cleiton Melo da Silva <cleiton.melo@gmail.com>
2019-09-23 17:34:56 -0300

    Organização de código com o repository

--- src/controllers/mentions-controller.js
+++ src/controllers/mentions-controller.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Mentions = mongoose.model('Mentions');
 
+const repository = require('../repositories/mentions-repository');
+
 // Lista total
 exports.listMentions = async (req, res) => {
     try {
-      const data = await Mentions.find({},'friend mention -id');
+      const data = await repository.listMentions();
       res.status(200).send(data);
     } catch (e) {
       res.status(500).send({message: 'Falha ao carregar as menções.'});
@@ -14,11 +16,10 @@ exports.listMentions = async (req, res) => {
 // Criação
 exports.createMention = async (req, res) => {
     try {
-        const mention = new Mentions({
+        await repository.createMention({
             friend: req.body.friend,
             mention: req.body.mention
         });
-        await mention.save();
         res.status(201).send({message: 'Menção cadastrada com sucesso!'});
     } catch (e) {
         res.status(500).send({message: 'Falha ao cadastrar a menção.'});  
